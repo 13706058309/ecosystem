66f3f3a281a13863c1a0affc7a04ad738f1d6671
HP <1246645590@qq.com>
2020-11-10 17:27:45 +0800

    主页

--- src/main/java/com/cykj/controller/HomePageContorller.java
+++ src/main/java/com/cykj/controller/HomePageContorller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/homePage")
-public class HomePage {
+public class HomePageContorller {
     @RequestMapping("/home")
     public String home(){
         return "HomePage";
