4a719229213262f9bae5438f8354e0e9b5a23663
汤某某 <2673142878@qq.com>
2020-11-11 09:17:22 +0800

    1

--- src/main/java/com/cykj/controller/HomePageContorller.java
+++ src/main/java/com/cykj/controller/HomePageContorller.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomePageContorller {
     @RequestMapping("/home")
     public String home(){
+
         return "HomePage";
     }
 }
