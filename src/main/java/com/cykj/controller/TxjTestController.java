361a07e1ea51d2ca85ab5ad925f6fffab6701636
汤某某 <2673142878@qq.com>
2020-11-13 09:40:52 +0800

    9.40

--- src/main/java/com/cykj/controller/TxjTestController.java
+++ src/main/java/com/cykj/controller/TxjTestController.java
@@ -1,5 +1,6 @@
 package com.cykj.controller;
 
+import com.cykj.entity.Course;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,5 +12,13 @@ public class TxjTestController {
         System.out.println("走入汤某人的测试控制类，即将访问 后台课程管理的JSP！！");
 
         return "BackCourseManagement";
+    }
+
+    @RequestMapping("findCourse")
+    public String findCourse(){
+
+        Course course =
+
+                return null;
     }
 }
---
+++ src/main/java/com/cykj/entity/Course.java
@@ -0,0 +1,93 @@
+package com.cykj.entity;
+
+public class Course {
+    private int courseId;
+    private String courseName;
+    private String ourseImgURL;
+    private String speakerName;
+    private String courseIntroduce;
+    private String uploadTime;
+    private int collectionNumber;
+    private int filedId;
+    private String speakerHeadImgURL;
+
+
+
+
+
+
+
+
+    //--------------------------    getset方法    --------------------------\\
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getOurseImgURL() {
+        return ourseImgURL;
+    }
+
+    public void setOurseImgURL(String ourseImgURL) {
+        this.ourseImgURL = ourseImgURL;
+    }
+
+    public String getSpeakerName() {
+        return speakerName;
+    }
+
+    public void setSpeakerName(String speakerName) {
+        this.speakerName = speakerName;
+    }
+
+    public String getCourseIntroduce() {
+        return courseIntroduce;
+    }
+
+    public void setCourseIntroduce(String courseIntroduce) {
+        this.courseIntroduce = courseIntroduce;
+    }
+
+    public String getUploadTime() {
+        return uploadTime;
+    }
+
+    public void setUploadTime(String uploadTime) {
+        this.uploadTime = uploadTime;
+    }
+
+    public int getCollectionNumber() {
+        return collectionNumber;
+    }
+
+    public void setCollectionNumber(int collectionNumber) {
+        this.collectionNumber = collectionNumber;
+    }
+
+    public int getFiledId() {
+        return filedId;
+    }
+
+    public void setFiledId(int filedId) {
+        this.filedId = filedId;
+    }
+
+    public String getSpeakerHeadImgURL() {
+        return speakerHeadImgURL;
+    }
+
+    public void setSpeakerHeadImgURL(String speakerHeadImgURL) {
+        this.speakerHeadImgURL = speakerHeadImgURL;
+    }
+}
