ce21669768c5814f0a617ad2b6eb324ed2b2fab9
MengXing2626 <197614515@qq.com>
2020-11-16 14:30:34 +0800

    保存

--- src/main/java/com/cykj/service/PowerService.java
+++ src/main/java/com/cykj/service/PowerService.java
@@ -9,6 +9,7 @@ import com.cykj.entity.RoleInfo;
 import java.util.List;
 
 public interface PowerService {
+
     public List<Menu> findExistMenu(long roleId);
     public List<Menu> findNotExistMenu(long roleId);
     public int addPower(long roleId, List<String> menuIds);
