60aa1947b06741c4febce7117711165b85eb14a9
lrk <836130506@qq.com>
2020-12-01 13:35:21 +0800

    2020年12月1日09:49:23

--- src/main/java/com/cykj/DemoApplication.java
+++ src/main/java/com/cykj/DemoApplication.java
@@ -5,11 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
 
 @SpringBootApplication
 @MapperScan("com.cykj.mapper")
-public class DemoApplication extends SpringBootServletInitializer {
+public class DemoApplication extends SpringBootServletInitializer  implements WebApplicationInitializer {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
